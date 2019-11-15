2811e00749cb3861fedfe31dd7f68c9ef4de7a5c
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-11-15 18:11:59 +0200

    Updates

---
+++ precache-manifest.644abdf4ba70d825b92c9194a1188e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee517bbf42ebb02508755ad220ddf62",
+    "url": "/goit-react-03-image-finder/index.html"
+  },
+  {
+    "revision": "1e055f29de9e5596815c",
+    "url": "/goit-react-03-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "563b432a68cbc95a52d1",
+    "url": "/goit-react-03-image-finder/static/css/main.59251a9b.chunk.css"
+  },
+  {
+    "revision": "1e055f29de9e5596815c",
+    "url": "/goit-react-03-image-finder/static/js/2.862ae96f.chunk.js"
+  },
+  {
+    "revision": "563b432a68cbc95a52d1",
+    "url": "/goit-react-03-image-finder/static/js/main.32d48bc1.chunk.js"
+  },
+  {
+    "revision": "180bf7e7d488c5d356bf",
+    "url": "/goit-react-03-image-finder/static/js/runtime-main.8688aa17.js"
+  }
+]);
